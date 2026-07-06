9b7123f475905d74f42c2ef1e8723c29691fd03e
JonatasGdeC <jonatas.07022003@gmail.com>
2026-07-06 20:39:05 -0300

    Criando entidade Bill

---
+++ src/Backend/PersonalFinance.Domain/Entities/Bill.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace PersonalFinance.Domain.Entities;
+
+public class Bill
+{
+    public long Id { get; set; }
+    public double Amount { get; set; }
+    public int Installments { get; set; }
+    
+    public long? CategoryId { get; set; }
+    public Category? Category { get; set; }
+    
+    public long ParticipantId { get; set; }
+    public required Participant Participant { get; set; }
+    
+    public Guid UserId { get; set; }
+    public required User User { get; set; }
+}
