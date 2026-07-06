94e8f29470e4e4fefb7e8e8c8d05087422e3034a
JonatasGdeC <jonatas.07022003@gmail.com>
2026-07-06 19:36:40 -0300

    first commit

---
+++ src/Web/PersonalFinance.Web/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PersonalFinance.Web;
+
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args: args);
+builder.RootComponents.Add<App>(selector: "#app");
+builder.RootComponents.Add<HeadOutlet>(selector: "head::after");
+
+builder.Services.AddScoped(implementationFactory: sp => new HttpClient { BaseAddress = new Uri(uriString: builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
