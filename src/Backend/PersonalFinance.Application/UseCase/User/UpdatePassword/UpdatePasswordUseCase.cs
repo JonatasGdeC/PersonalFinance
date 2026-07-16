543b43a9a0a1fe192d1b9ce42667f0938d5c3be8
JonatasGdeC <jonatas.07022003@gmail.com>
2026-07-15 21:00:16 -0300

    Criando lógica de update da senha do user em useState

--- src/Backend/PersonalFinance.Application/UseCase/User/UpdatePassword/UpdatePasswordUseCase.cs
+++ src/Backend/PersonalFinance.Application/UseCase/User/UpdatePassword/UpdatePasswordUseCase.cs
@@ -1,8 +1,49 @@
+using FluentValidation.Results;
 using PersonalFinance.Communication.Requests.User;
+using PersonalFinance.Domain.Repositories;
+using PersonalFinance.Domain.Repositories.User;
+using PersonalFinance.Domain.Security.Cryptography;
+using PersonalFinance.Domain.Services.LoggedUser;
+using PersonalFinance.Exception;
+using PersonalFinance.Exception.ExceptionBase;
 
 namespace PersonalFinance.Application.UseCase.User.UpdatePassword;
+using Domain.Entities;
 
-public class UpdatePasswordUseCase : IUpdatePasswordUseCase
+public class UpdatePasswordUseCase(
+    ILoggedUser loggedUser,
+    IEncrypter encrypter,
+    IUserWriteRepository writeRepository,
+    IUnitOfWork unitOfWork) : IUpdatePasswordUseCase
 {
-    public Task Execute(UpdatePasswordRequest request) => throw new NotImplementedException();
+    public async Task Execute(UpdatePasswordRequest request)
+    {
+        User user = await loggedUser.Get();
+        
+        Validate(request: request, user: user);
+        
+        user.Password = encrypter.Encrypt(value: request.NewPassword);
+        writeRepository.Update(user: user);
+        await unitOfWork.Commit();
+    }
+    
+    private void Validate(UpdatePasswordRequest request, User user)
+    {
+        ValidationResult resultPassword = new PasswordValidator().Validate(instance: request.NewPassword);
+
+        if (!resultPassword.IsValid)
+        {
+            List<string> errors = resultPassword.Errors.Select(selector: error => error.ErrorMessage).ToList();
+            throw new ErrorOnValidationException(errorsMessages: errors);
+        }
+
+        if (!string.IsNullOrEmpty(value: user.Password))
+        {
+            bool passwordMatch = encrypter.Verify(value: request.OldPassword, hash: user.Password);
+            if (!passwordMatch)
+            {
+                throw new BadRequestException(message: ResourceErrorMessages.OLD_PASSWORD_INVALID);
+            }
+        }
+    }
 }
