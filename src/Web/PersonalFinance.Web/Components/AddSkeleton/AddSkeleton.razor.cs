5e06be2bebc4660f0b92e052dcfc12f5a2c21e02
JonatasGdeC <jonatas.07022003@gmail.com>
2026-07-16 21:55:17 -0300

    Criando AddSkeleton

---
+++ src/Web/PersonalFinance.Web/Components/AddSkeleton/AddSkeleton.razor.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components;
+
+namespace PersonalFinance.Web.Components.AddSkeleton;
+
+public partial class AddSkeleton
+{
+  [Parameter] public string Width { get; set; } = "100%";
+  [Parameter] public string Height { get; set; } = "5px";
+  [Parameter] public string BorderRadius { get; set; } = "5px";
+
+  private string HandleStyle()
+  {
+    string width = $"width: {Width};";
+    string height = $"height: {Height};";
+    string radius = $"radius: {BorderRadius};";
+
+    return $"{width} {height} {radius}";
+  }
+}
