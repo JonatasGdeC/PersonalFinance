1f412c66b075c5359cea8dc2b2c60c2c5f4bfe70
JonatasGdeC <jonatas.07022003@gmail.com>
2026-07-21 17:12:34 -0300

    Criando SummaryMain.razor e aplicando em home

---
+++ src/Web/PersonalFinance.Web/Pages/Home/Components/SummaryMain/SummaryMain.razor.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Components;
+
+namespace PersonalFinance.Web.Pages.Home.Components.SummaryMain;
+
+public partial class SummaryMain : ComponentBase
+{
+    private static readonly CultureInfo CurrencyCulture = new(name: "en-US");
+
+    [Parameter] public bool IsLoading { get; init; }
+    [Parameter] public double? CurrentBalance { get; init; }
+    [Parameter] public double? TotalIncome { get; init; }
+    [Parameter] public double? TotalExpense { get; init; }
+
+    private string FormattedCurrentBalance => FormatCurrency(value: CurrentBalance);
+    private string FormattedTotalIncome => FormatCurrency(value: TotalIncome);
+    private string FormattedTotalExpense => FormatCurrency(value: TotalExpense);
+
+    private static string FormatCurrency(double? value) => (value ?? 0).ToString(format: "C2", provider: CurrencyCulture);
+}
--- src/Web/PersonalFinance.Web/Pages/Home/Main/Home.razor.cs
+++ src/Web/PersonalFinance.Web/Pages/Home/Main/Home.razor.cs
@@ -1,6 +1,15 @@
+using PersonalFinance.Communication.Responses.Transaction;
+
 namespace PersonalFinance.Web.Pages.Home.Main;
 
 public partial class Home
 {
-    
+    private bool _isLoading = true;
+    private GetTransactionDashboardResponse? _transactionDashboard;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _transactionDashboard = await PersonalFinanceApi.Transaction.GetDashboard(date: DateTime.Today);
+        _isLoading = false;
+    }
 }
