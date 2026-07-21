e3fe9100fb535c31f3fa358fde3d976258c3d3c0
JonatasGdeC <jonatas.07022003@gmail.com>
2026-07-21 17:12:14 -0300

    Ajustes em AddCard.razor.cs para pegar cor diretamente de enum

--- src/Web/PersonalFinance.Web/Components/AddCard/AddCard.razor.cs
+++ src/Web/PersonalFinance.Web/Components/AddCard/AddCard.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using PersonalFinance.Web.Extensions;
 
 namespace PersonalFinance.Web.Components.AddCard;
 
@@ -8,7 +9,7 @@ public partial class AddCard : ComponentBase
     [Parameter] public string Width { get; set; } = "auto";
     [Parameter] public string Height { get; set; } = "auto";
     [Parameter] public string BorderRadius { get; set; } = "12px";
-    [Parameter] public string BackgroundColor { get; set; } = "var(--white)";
+    [Parameter] public ThemeColor BackgroundColor { get; set; } = ThemeColor.White;
     [Parameter] public string Padding { get; set; } = "32px";
     [Parameter] public bool IsLoading { get; set; }
 
@@ -17,7 +18,7 @@ public partial class AddCard : ComponentBase
         string width = $"width: {Width};";
         string height = $"height: {Height};";
         string borderRadius = $"border-radius: {BorderRadius};";
-        string backgroundColor = $"background-color: {BackgroundColor};";
+        string backgroundColor = $"background-color: {BackgroundColor.ToCssVar()};";
         string padding = $"padding: {Padding};";
 
         return $"{width} {height} {borderRadius} {backgroundColor} {padding}";
