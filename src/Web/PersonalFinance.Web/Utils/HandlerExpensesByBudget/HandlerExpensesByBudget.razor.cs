0bbf65fe5c893be39395af86357258ba40a26a80
JonatasGdeC <jonatas.07022003@gmail.com>
2026-07-24 21:39:07 -0300

    refactor: refactor em HandlerExpensesByBudget para checagem de currentDate

--- src/Web/PersonalFinance.Web/Utils/HandlerExpensesByBudget/HandlerExpensesByBudget.razor.cs
+++ src/Web/PersonalFinance.Web/Utils/HandlerExpensesByBudget/HandlerExpensesByBudget.razor.cs
@@ -5,22 +5,36 @@ using PersonalFinance.Communication.Responses.Transaction;
 
 namespace PersonalFinance.Web.Utils.HandlerExpensesByBudget;
 
-public partial class HandlerExpensesByBudget : ComponentBase
+public partial class HandlerExpensesByBudget : ComponentBase, IDisposable
 {
     [Parameter] public required BudgetDto Budget { get; init; }
-    
+
     private bool _isLoading = true;
     private GetListTransactionsResponse? _transactions;
-    
+
     protected override async Task OnInitializedAsync()
     {
-        _transactions = await PersonalFinanceApi.Transaction.GetByCategory(categoryId: Budget.Category.Id, date: DateTime.Now,
+        DateState.StateChanged += HandleDateChanged;
+        await LoadTransactions();
+    }
+
+    private async void HandleDateChanged(object? sender, EventArgs e) => await InvokeAsync(workItem: LoadTransactions);
+
+    private async Task LoadTransactions()
+    {
+        _isLoading = true;
+        StateHasChanged();
+
+        _transactions = await PersonalFinanceApi.Transaction.GetByCategory(categoryId: Budget.Category.Id, date: DateState.Value.CurrentDate,
             pagination: new PaginationRequest
             {
                 PageNumber = 1,
                 PageSize = 3
             });
-        
+
         _isLoading = false;
+        StateHasChanged();
     }
+
+    public void Dispose() => DateState.StateChanged -= HandleDateChanged;
 }
