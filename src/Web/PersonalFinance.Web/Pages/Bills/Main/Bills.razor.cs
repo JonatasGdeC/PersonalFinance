ceccf178e44e7db5b0ddde829bb1ea4949201047
JonatasGdeC <jonatas.07022003@gmail.com>
2026-07-24 21:39:52 -0300

    feature: criando componente principal de páginas que faltavam

---
+++ src/Web/PersonalFinance.Web/Pages/Bills/Main/Bills.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace PersonalFinance.Web.Pages.Bills.Main;
+
+public partial class Bills : ComponentBase
+{
+}
---
+++ src/Web/PersonalFinance.Web/Pages/Pots/Main/Pots.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace PersonalFinance.Web.Pages.Pots.Main;
+
+public partial class Pots : ComponentBase
+{
+}
