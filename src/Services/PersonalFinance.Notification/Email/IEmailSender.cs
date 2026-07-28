07b5f44cd3fccbb85aac7e81ccc7a464d1f6bf75
JonatasGdeC <jonatas.07022003@gmail.com>
2026-07-27 21:43:26 -0300

    Refactor: removendo serviço

--- src/Services/PersonalFinance.Notification/Email/IEmailSender.cs
+++
@@ -1,6 +0,0 @@
-namespace PersonalFinance.Notification.Email;
-
-public interface IEmailSender
-{
-    Task SendAsync(string toEmail, string toName, string subject, string body, CancellationToken cancellationToken);
-}
