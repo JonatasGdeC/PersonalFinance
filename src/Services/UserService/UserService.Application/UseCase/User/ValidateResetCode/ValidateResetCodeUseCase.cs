900d0715f6fe2f56d4fe9ff2d569172424199677
JonatasGdeC <jonatas.07022003@gmail.com>
2026-08-01 20:20:26 -0300

    feature: criando ValidateResetCodeUseCase

--- src/Services/UserService/UserService.Application/UseCase/User/ValidateResetCode/ValidateResetCodeUseCase.cs
+++ src/Services/UserService/UserService.Application/UseCase/User/ValidateResetCode/ValidateResetCodeUseCase.cs
@@ -1,9 +1,48 @@
 using PersonalFinance.Communication.Requests.User;
 using PersonalFinance.Communication.Responses.User;
+using PersonalFinance.Exception;
+using PersonalFinance.Exception.ExceptionBase;
+using UserService.Domain.Repositories;
+using UserService.Domain.Repositories.PasswordResetCode;
+using UserService.Domain.Repositories.User;
+using UserService.Domain.Security.Cryptography;
+using UserService.Domain.Security.Tokens;
 
 namespace UserService.Application.UseCase.User.ValidateResetCode;
+using Domain.Entities;
 
-public class ValidateResetCodeUseCase : IValidateResetCodeUseCase
+public class ValidateResetCodeUseCase(
+    IUserReadRepository userReadRepository,
+    IPasswordResetCodeRepository passwordResetCodeRepository,
+    IEncrypter encrypter,
+    IPasswordResetTokenGenerator passwordResetTokenGenerator,
+    IUnitOfWork unitOfWork) : IValidateResetCodeUseCase
 {
-    public Task<ValidateResetCodeResponse> Execute(ValidateResetCodeRequest request) => throw new NotImplementedException();
+    public async Task<ValidateResetCodeResponse> Execute(ValidateResetCodeRequest request)
+    {
+        User user = await userReadRepository.GetByEmail(email: request.Email) ??
+                    throw new BadRequestException(message: ResourceErrorMessages.CODE_INVALID);
+        
+        PasswordResetCode passwordResetCode = await passwordResetCodeRepository.GetByUserId(userId: user.Id) ??
+                                              throw new BadRequestException(message: ResourceErrorMessages.CODE_INVALID);
+
+        if (!passwordResetCode.IsValid)
+        {
+            throw new BadRequestException(message: ResourceErrorMessages.CODE_INVALID);
+        }
+
+        bool isValidCode = encrypter.Verify(value: request.Code, hash: passwordResetCode.CodeHash);
+        if (!isValidCode)
+        {
+            passwordResetCode.Attempts++;
+            passwordResetCodeRepository.Update(passwordResetCode: passwordResetCode);
+            await unitOfWork.Commit();
+            throw new BadRequestException(message: ResourceErrorMessages.CODE_INVALID);
+        }
+
+        return new ValidateResetCodeResponse
+        {
+            TokenResetPassword = passwordResetTokenGenerator.Generate(user: user)
+        };
+    }
 }
